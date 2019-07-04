e7c70575de3b1f501a57ca0ed7180f717b7e6855
xaero <19_5@bk.ru>
2019-07-04 06:39:04 -0400

    5_2!!yeah!

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -23,11 +23,13 @@ ip_net = input('input ip-network like a.a.a.a/b ')
 help1 = ip_net.split('/')
 help2 = help1[0].split('.')
 help3 = int(help1[1])
-
-
+help4 = 32 - help3
+mask = str('1'*help3) + str('0'*help4)
 
 print('Network:\n{:<12}{:<12}{:<12}{:<12}'.format(int(help2[0]),int(help2[1]),int(help2[2]),int(help2[3])))
-print('{:<012b}{:<012b}{:<012b}{:<012b}'.format(int(help2[0]),int(help2[1]),int(help2[2]),int(help2[3])))
+print('{:<08b}    {:<08b}    {:<08b}    {:<08b}'.format(int(help2[0]),int(help2[1]),int(help2[2]),int(help2[3])))
 print('\n')
 print('Mask:\n/{}'.format(help1[1]))
+print('{:<12}{:<12}{:<12}{:<12}'.format(int(mask[0:8],2),int(mask[8:16],2),int(mask[16:24],2),int(mask[24:32],2)))
+print('{:<12}{:<12}{:<12}{:<12}'.format(int(mask[0:8]),int(mask[8:16]),int(mask[16:24]),int(mask[24:32])))
 
