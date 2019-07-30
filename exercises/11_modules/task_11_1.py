533126cbd313139ef5ee59995c6edfcaf66f2547
xaero <19_5@bk.ru>
2019-07-30 05:28:02 -0400

    11_1 success

--- exercises/11_modules/task_11_1.py
+++ exercises/11_modules/task_11_1.py
@@ -28,3 +28,34 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
 
+
+def parse_cdp_neighbors(command_output):
+    result = {}
+    com_out = command_output.split()
+    full_count = 0-(len(com_out))
+    first_elem1 = com_out[0].split('>')
+    first_elem = first_elem1[0]
+    counter = 2
+    for k in range(-1, full_count, -1):
+        if 'ID' in com_out[k]:
+            break
+        elif '/' in com_out[k] and counter == 2:
+            elem2 = com_out[k-1]+com_out[k]
+            counter = 1
+        elif '/' in com_out[k] and counter == 1:
+            elem1 = com_out[k-1]+com_out[k]
+            counter = 2
+            switch_elem = com_out[k-2]
+            list1 = [first_elem, elem1]
+            tuple1 = tuple(list1)
+            list2 = [switch_elem, elem2]
+            tuple2 = tuple(list2)
+            result1 = {tuple1: tuple2}
+            result.update(result1)
+    return result
+                
+
+
+with open('sh_cdp_n_sw1.txt', 'r') as sc:
+    source = sc.read()
+    print(parse_cdp_neighbors(source))
