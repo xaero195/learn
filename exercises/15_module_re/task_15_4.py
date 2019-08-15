d4c590db28a6df3d21b4218c3c8d6761c47f1183
xaero <19_5@bk.ru>
2019-08-15 05:12:53 -0400

    15_5 for finish

--- exercises/15_module_re/task_15_4.py
+++ exercises/15_module_re/task_15_4.py
@@ -22,4 +22,22 @@ interface Loopback0
 
 Проверить работу функции на примере файла config_r1.txt.
 '''
+import re
 
+def get_ints_without_description(filename):
+    result = []
+    regex1 = re.compile('^interface (\S+)')
+    regex2 = re.compile('^ description')
+    with open(filename) as f:
+        for line in f:
+            match1 = regex1.search(line)
+            if match1:
+                interface = match1.group(1)
+                result.append(interface)
+            match2 = regex2.search(line)
+            if match2:
+                result.pop()
+    return result
+
+if __name__=='__main__':
+    print(get_ints_without_description('config_r1.txt'))
