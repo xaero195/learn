5fdf0b4e8917344eccc70fa9d3bb93e784ce5f7b
xaero <19_5@bk.ru>
2019-08-22 10:29:46 -0400

    will be 17_2b

--- exercises/17_serialization/task_17_2a.py
+++ exercises/17_serialization/task_17_2a.py
@@ -32,3 +32,43 @@
 Проверить работу параметра save_to_filename и записать итоговый словарь в файл topology.yaml.
 
 '''
+import re
+import yaml
+
+def parse_sh_cdp_neighbors(source_line):
+    name_dev_regex = re.compile('(\S+)>show')
+    interface_regex = re.finditer('(\S+)\s+((?:Fa|Eth) \S+).+?((?:Fa|Eth) \S+)',source_line)
+    match_dev = name_dev_regex.search(source_line)
+    #print(interface_regex)
+    if match_dev:
+        key = match_dev.group(1)
+    if interface_regex:
+        dict3 = {}
+    for match_int in interface_regex:
+        dict1 = {match_int.group(1): match_int.group(3)}
+        dict2 = {match_int.group(2): dict1}
+        dict3.update(dict2)
+    dict4 = {key: dict3}
+    return dict4
+
+def generate_topology_from_cdp(list_of_files, save_to_filename = None):
+    result1 = {}
+    for files in list_of_files:
+        with open(files,'r') as f:
+            source = f.read()
+            dict_parse = parse_sh_cdp_neighbors(source)
+            result1.update(dict_parse)
+    if save_to_filename:
+        with open(save_to_filename, 'w') as w:
+            yaml.dump(result1, w)
+    return result1
+
+if __name__=='__main__':
+    list_files = ['sh_cdp_n_sw1.txt',
+                  'sh_cdp_n_r1.txt',
+                  'sh_cdp_n_r2.txt',
+                  'sh_cdp_n_r3.txt',
+                  'sh_cdp_n_r4.txt',
+                  'sh_cdp_n_r5.txt',
+                  'sh_cdp_n_r6.txt']
+    generate_topology_from_cdp(list_files, 'topology.yaml')
