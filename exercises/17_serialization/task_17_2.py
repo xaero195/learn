728728b4f48013fa149c435d4c785bc273944287
xaero <19_5@bk.ru>
2019-08-22 10:08:45 -0400

    last all inquisitions with re 17_2 complit

--- exercises/17_serialization/task_17_2.py
+++ exercises/17_serialization/task_17_2.py
@@ -28,19 +28,18 @@ import re
 
 def parse_sh_cdp_neighbors(source_line):
     name_dev_regex = re.compile('(\S+)>show')
-    interface_regex = re.finditer('(\S+) +(Fa|Eth +\S+) +\.?+(Fa|Eth \S+)',source_line)
+    interface_regex = re.finditer('(\S+)\s+((?:Fa|Eth) \S+).+?((?:Fa|Eth) \S+)',source_line)
     match_dev = name_dev_regex.search(source_line)
     print(interface_regex)
     if match_dev:
         key = match_dev.group(1)
     if interface_regex:
         dict3 = {}
-        for match in interface_regex:
-            print(match)
-            dict1 = {match.group(1): match.group(3)}
-            dict2 = {match.group(2): dict1}
-            dict3.append(dict2)
-        dict4 = {key: dict3}
+    for match_int in interface_regex:
+        dict1 = {match_int.group(1): match_int.group(3)}
+        dict2 = {match_int.group(2): dict1}
+        dict3.update(dict2)
+    dict4 = {key: dict3}
     return dict4
 
 if __name__=='__main__':
