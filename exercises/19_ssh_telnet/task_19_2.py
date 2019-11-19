715f6dc3f55063ddca1c349b1ec335d40130173d
xaero <19_5@bk.ru>
2019-11-19 05:50:37 -0500

    stop on 19_2b

--- exercises/19_ssh_telnet/task_19_2.py
+++ exercises/19_ssh_telnet/task_19_2.py
@@ -41,6 +41,51 @@ R1#
 Скрипт должен отправлять команду command на все устройства из файла devices.yaml с помощью функции send_config_commands.
 '''
 
+import getpass
+import sys
+import netmiko
+import yaml
+
+#command = sys.argv[1]
+#user = input('Username: ')
+#password = getpass.getpass()
+#enable_pass = getpass.getpass(prompt='Enter enable password: ')
+#devices_ip = ['192.168.100.1', '192.168.100.2', '192.168.100.3']
+filename = 'devices.yaml'
+#command = 'sh ip int br'
 commands = [
     'logging 10.255.255.1', 'logging buffered 20010', 'no logging console'
 ]
+
+def send_show_command(device, command):
+    try:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+
+            result = ssh.send_command(command)
+#        print(result)
+    except error_authen:
+        print('ERROR PASSWORD, ITIT KOLOTIT!')
+    except ip_address:
+        print('ERROR IP, ITIT KOLOTIT!')
+    return result
+
+def send_config_commands(device, config_commands):
+    try:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+
+            result = ssh.send_config_set(config_commands)
+#        print(result)
+    except error_authen:
+        print('ERROR PASSWORD, ITIT KOLOTIT!')
+    except ip_address:
+        print('ERROR IP, ITIT KOLOTIT!')
+    return result
+
+with open(filename) as f:
+    devices = yaml.safe_load(f)
+for item in devices:
+#    print(send_show_command(item, command))
+    print(send_config_commands(item, commands))
+
--- exercises/19_ssh_telnet/task_19_2a.py
+++ exercises/19_ssh_telnet/task_19_2a.py
@@ -21,3 +21,53 @@ In [15]:
 
 Скрипт должен отправлять список команд commands на все устройства из файла devices.yaml с помощью функции send_config_commands.
 '''
+
+import getpass
+import sys
+import netmiko
+import yaml
+
+#command = sys.argv[1]
+#user = input('Username: ')
+#password = getpass.getpass()
+#enable_pass = getpass.getpass(prompt='Enter enable password: ')
+#devices_ip = ['192.168.100.1', '192.168.100.2', '192.168.100.3']
+filename = 'devices.yaml'
+#command = 'sh ip int br'
+commands = [
+    'logging 10.255.255.1', 'logging buffered 20010', 'no logging console'
+]
+
+def send_show_command(device, command):
+    try:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+
+            result = ssh.send_command(command)
+#        print(result)
+    except error_authen:
+        print('ERROR PASSWORD, ITIT KOLOTIT!')
+    except ip_address:
+        print('ERROR IP, ITIT KOLOTIT!')
+    return result
+
+def send_config_commands(device, config_commands, verbose=True):
+    try:
+        if verbose:
+            print('Try to connect with {}'.format(device[ip]))
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+
+            result = ssh.send_config_set(config_commands)
+#        print(result)
+    except error_authen:
+        print('ERROR PASSWORD, ITIT KOLOTIT!')
+    except ip_address:
+        print('ERROR IP, ITIT KOLOTIT!')
+    return result
+
+with open(filename) as f:
+    devices = yaml.safe_load(f)
+for item in devices:
+#    print(send_show_command(item, command))
+    print(send_config_commands(item, commands))
