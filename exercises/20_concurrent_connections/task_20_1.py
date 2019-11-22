2bf4e385205e882c3cfe59c3939b8badcffa71a3
xaero <19_5@bk.ru>
2019-11-22 03:34:21 -0500

    all possible 20 finished

--- exercises/20_concurrent_connections/task_20_1.py
+++ exercises/20_concurrent_connections/task_20_1.py
@@ -17,3 +17,43 @@
 
 Для проверки доступности IP-адреса, используйте ping.
 '''
+
+from concurrent.futures import ThreadPoolExecutor
+import subprocess
+
+ip_list_source = []
+limit_source = 3
+
+for i in range(0,255):
+    ip_address = '172.19.30.' + str(i)
+    ip_list_source.append(ip_address)
+
+def ping(ip_address):
+    reply = subprocess.run(['ping', '-c', '3', '-n', ip_address],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                encoding='utf-8')
+    if reply.returncode == 0:
+        return True
+    else:
+        return False
+
+def ping_ip_addresses(ip_list, limit=3):
+    good = []
+    bad = []
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        result1 = executor.map(ping, ip_list)
+        for ip, output in zip(ip_list, result1):
+            if output:
+                good.append(ip)
+            else:
+                bad.append(ip)
+    result = tuple([good, bad])
+    return result
+
+if __name__=='__main__':
+    print(ping_ip_addresses(ip_list_source, limit_source))
+
+
+
+
