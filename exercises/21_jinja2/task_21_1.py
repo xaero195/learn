967d5d1340f4b668908dd4673e80a452c89ddeef
xaero <19_5@bk.ru>
2019-11-25 02:41:24 -0500

    21_1

--- exercises/21_jinja2/task_21_1.py
+++ exercises/21_jinja2/task_21_1.py
@@ -13,3 +13,27 @@
 Проверить работу функции на шаблоне templates/for.txt и данных из файла data_files/for.yml.
 
 '''
+import yaml
+from jinja2 import Environment, FileSystemLoader
+import sys
+import os
+
+template_path = 'templates/for.txt'
+data_path = 'data_files/for.yml'
+
+def generate_config(template, data_dict):
+    template_dir, template_file = os.path.split(template)
+    env = Environment(
+        loader=FileSystemLoader(template_dir),
+        trim_blocks=True,
+        lstrip_blocks=True)
+    template_final = env.get_template(template_file)
+
+    with open(data_dict) as f:
+        vars_dict = yaml.safe_load(f)
+
+    return template_final.render(vars_dict)
+
+
+if __name__=='__main__':
+    print(generate_config(template_path, data_path))
