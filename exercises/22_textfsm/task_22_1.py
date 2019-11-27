02b3976f66a27d32c717829dd0cbe8923350e314
xaero <19_5@bk.ru>
2019-11-27 03:50:47 -0500

    stop on 22_2

--- exercises/22_textfsm/task_22_1.py
+++ exercises/22_textfsm/task_22_1.py
@@ -14,3 +14,21 @@
 
 '''
 
+import sys
+import textfsm
+from tabulate import tabulate
+
+def parse_command_output(template, command_output):
+    with open(template) as f:
+        re_table = textfsm.TextFSM(f)
+        header = re_table.header
+        body = re_table.ParseText(command_output)
+        body.insert(0, header)
+        result = body
+    return result
+
+if __name__=='__main__':
+    with open('output/sh_ip_int_br.txt') as o:
+        output = o.read()
+        print(parse_command_output('templates/sh_ip_int_br.template', output))
+        
--- exercises/22_textfsm/task_22_1a.py
+++ exercises/22_textfsm/task_22_1a.py
@@ -14,3 +14,37 @@
 
 Проверить работу функции на выводе команды output/sh_ip_int_br.txt и шаблоне templates/sh_ip_int_br.template.
 '''
+
+import sys
+import textfsm
+from tabulate import tabulate
+
+def parse_command_output(template, command_output):
+    with open(template) as f:
+        re_table = textfsm.TextFSM(f)
+        header = re_table.header
+        body = re_table.ParseText(command_output)
+        body.insert(0, header)
+        result = body
+    return result
+
+def parse_output_to_dict(template, command_output):
+    result = []
+    with open(template) as f:
+        re_table = textfsm.TextFSM(f)
+        header = re_table.header
+        body = re_table.ParseText(command_output)
+        lenght_header = len(header)
+        lenght_body = len(body)
+        for i in range(lenght_body):
+            result1 = {}
+            for j in range(lenght_header):
+                result1[header[j]]=body[i][j]
+            result.append(result1)
+    return result
+
+if __name__=='__main__':
+    with open('output/sh_ip_int_br.txt') as o:
+        output = o.read()
+        print(parse_command_output('templates/sh_ip_int_br.template', output))
+        print(parse_output_to_dict('templates/sh_ip_int_br.template', output))
